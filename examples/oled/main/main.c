f014a60bc59bfdc101bb6202e1f304ddcbf66b7d
Ayush Kaura <ayushkaura@gmail.com>
2022-10-27 23:26:23 +0530

    WIP oled code using lvgl
    Cleaned oled example file

--- examples/oled/main/main.c
+++ examples/oled/main/main.c
@@ -30,34 +30,7 @@ static const char *TAG = "oled_example";
 void app_main()
 {
 
-    // init_oled();
-
-	lv_init();
-	
-	/* Initialize I2C bus used by the drivers */
-	lvgl_i2c_driver_init(I2C_NUM_0, OLED_SDA, OLED_SCL, OLED_IIC_FREQ_HZ);
-	ssd1306_init();
-
-	lv_color_t* buf1 = heap_caps_malloc(DISP_BUF_SIZE * sizeof(lv_color_t), MALLOC_CAP_DMA);
-
-    lv_disp_draw_buf_t disp_buf;
-
-    uint32_t size_in_px = DISP_BUF_SIZE; 
-	size_in_px *= 8;
-    
-    /* Initialize the working buffer depending on the selected display */
-    lv_disp_draw_buf_init(&disp_buf, buf1, NULL, size_in_px);
-
-    lv_disp_drv_t disp_drv;
-    lv_disp_drv_init(&disp_drv);
-	disp_drv.hor_res = 128;                 
-	disp_drv.ver_res = 64;
-    disp_drv.flush_cb = ssd1306_flush;
-    disp_drv.rounder_cb = ssd1306_rounder;
-    disp_drv.set_px_cb = ssd1306_set_px_cb;
-    disp_drv.draw_buf = &disp_buf;
-
-    lv_disp_t * disp = lv_disp_drv_register(&disp_drv);
+    init_oled();
 
 	ESP_LOGI(TAG, "Successfully displayed Hello World on the OLED Screen");
 }
