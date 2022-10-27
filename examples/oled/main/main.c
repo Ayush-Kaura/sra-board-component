e6ac7c9ea7537c48e04ec40ff7f8079c1448cdb5
Ayush Kaura <ayushkaura@gmail.com>
2022-10-27 18:53:03 +0530

    WIP oled code using lvgl

--- examples/oled/main/main.c
+++ examples/oled/main/main.c
@@ -27,24 +27,74 @@
 
 static const char *TAG = "oled_example";
 
+#define LV_TICK_PERIOD_MS 1
+// static void lv_tick_task(void *arg);
+
 void app_main()
 {
-	// Declaring an empty u8g2_t struct to configure the oled
-	u8g2_t oled_config;
 
-	// Initialising the OLED
-	init_oled(&oled_config);
-	vTaskDelay(100);
+    // init_oled();
 
-	// Clearing the screen
-	u8g2_ClearBuffer(&oled_config);
+	lv_init();
+	
+	/* Initialize I2C bus used by the drivers */
+	lvgl_i2c_driver_init(I2C_NUM_0, OLED_SDA, OLED_SCL, OLED_IIC_FREQ_HZ);
+	ssd1306_init();
 
-	// Setting the font and printing the string on screen
-	u8g2_SetFont(&oled_config, u8g2_font_ncenB14_tr);
-	u8g2_DrawStr(&oled_config, 2, 17, "Hello World!");
+	lv_color_t* buf1 = heap_caps_malloc(DISP_BUF_SIZE * sizeof(lv_color_t), MALLOC_CAP_DMA);
 
-	// Sending the buffer to the screen
-	u8g2_SendBuffer(&oled_config);
+    lv_disp_draw_buf_t disp_buf;
 
+    uint32_t size_in_px = DISP_BUF_SIZE; 
+	size_in_px *= 8;
+    
+    /* Initialize the working buffer depending on the selected display */
+    lv_disp_draw_buf_init(&disp_buf, buf1, NULL, size_in_px);
+
+    lv_disp_drv_t disp_drv;
+    lv_disp_drv_init(&disp_drv);
+	disp_drv.hor_res = 128;                 
+	disp_drv.ver_res = 64;
+    disp_drv.flush_cb = ssd1306_flush;
+    disp_drv.rounder_cb = ssd1306_rounder;
+    disp_drv.set_px_cb = ssd1306_set_px_cb;
+    disp_drv.draw_buf = &disp_buf;
+
+    lv_disp_t * disp = lv_disp_drv_register(&disp_drv);
+
+	// lv_theme_t * th = lv_theme_default_init(disp,  lv_color_black(), lv_color_white(), false, &lv_font_montserrat_14); 
+
+	// lv_disp_set_theme(disp, th); 
+
+	/* Create and start a periodic timer interrupt to call lv_tick_inc */
+    // const esp_timer_create_args_t periodic_timer_args = {
+    //     .callback = &lv_tick_task,
+    //     .name = "periodic_gui"
+    // };
+    // esp_timer_handle_t periodic_timer;
+    // ESP_ERROR_CHECK(esp_timer_create(&periodic_timer_args, &periodic_timer));
+    // ESP_ERROR_CHECK(esp_timer_start_periodic(periodic_timer, LV_TICK_PERIOD_MS * 1000));
+    // lv_obj_t *scr = lv_disp_get_scr_act(NULL);
+    // display_sra_logo(scr);
+    // lv_obj_clean(scr);
+    // lv_task_handler();
+    // display_walle_logo(scr);
+    // lv_task_handler();
+    display_servo_values(10, 10, 10, 10);
+    lv_task_handler();
+
+	while (1) {
+        /* Delay 1 tick (assumes FreeRTOS tick is 10ms */
+        
+        vTaskDelay(pdMS_TO_TICKS(100));
+        // lv_task_handler();
+	}
+	free(buf1);
 	ESP_LOGI(TAG, "Successfully displayed Hello World on the OLED Screen");
 }
+
+// static void lv_tick_task(void *arg) {
+//     (void) arg;
+
+//     lv_tick_inc(LV_TICK_PERIOD_MS);
+// }
